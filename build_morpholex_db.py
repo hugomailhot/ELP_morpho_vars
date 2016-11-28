5ccf0ff0bc9ab2fc2eb1a7c69674f59e38e39d4d
drumbot <mailhot.hugo@gmail.com>
2016-11-27 16:33:17 -0800

    Modified output to desired one for apply_morpho_vars_to_lex_db

--- build_morpholex_db.py
+++ build_morpholex_db.py
@@ -41,27 +41,33 @@ DB_PATH = os.path.join(PROJECT_PATH, '/home/hugo/Projects/ELP_morpho_vars/input/
 VARS_SAVE_PATH = os.path.join(PROJECT_PATH, 'output/ELP_morphological_variables.csv')
 HAPAX_SBTL_FREQ_THRESHOLD = 0.02
 HAPAX_HAL_FREQ_THRESHOLD = 1
+DB_ITEMID_COL = 0
 DB_WORD_COL = 1
-DB_SBTL_FREQ_COL = 27
-DB_HAL_FREQ_COL = 25
-DB_SEGM_COL = 48
 DB_POS_COL = 2
+DB_HAL_FREQ_COL = 25
+DB_SBTL_FREQ_COL = 27
+DB_SEGM_COL = 48
 
 
 def apply_morpho_vars_to_lex_db(db, morpho_vars):
     """
     Build a dict {PRS_signature: [lexical_data]}, where lexical_data is a list
-    of rows from the lexical database, each of which are expanded by including
-    morphological variables for each segmented morphemes.
+    of rows from the lexical database, each of which contain the data described
+    in the top comment of this script.
     """
     res = {}
     for row in db:
-        temp = copy.deepcopy(row)
+        # temp = copy.deepcopy(row)
         segm = row[DB_SEGM_COL]
         prs = get_PRS_signature(row[DB_SEGM_COL])
         if prs not in res.keys():
             res[prs] = []
         morphemes = get_morphemes(segm)
+        wordlen = len(row[DB_WORD_COL])
+        n_morphemes = len(morphemes)
+        prs_string = ','.join([str(x) for x in prs])
+        temp = (row[DB_ITEMID_COL] + row[DB_WORD_COL] + row[DB_POS_COL] + 
+                [wordlen, n_morphemes, prs_string, segm])
         for m in morphemes:
             freq = morpho_vars[m]['hal_freq']
             rel_fam_freq = get_relative_fam_freq(m, db, segm, freq,
@@ -213,6 +219,22 @@ def get_relative_fam_freq(morpheme, db, segm, word_freq, family):
 
 
 def merge_new_data_with_database(prs_data, main_db):
+    """
+    For each word in prs_data, put together the following data:
+    - ELP ItemID
+    - Word
+    - POS
+    - Word length
+    - Number of morphemes
+    - PRS signature
+    - MorphoLexSegm
+    - Morphological variables for each morpheme:
+      * Frequency
+      * Family size
+      * %MoreFreqInFam
+      * P
+      * P*
+    """
     for signature in prs_data:
         for i, row in enumerate(prs_data[signature]):
             word = row[0]
@@ -294,20 +316,20 @@ if __name__ == '__main__':
     print('computing morphological variables')
     morpho_vars = compute_morphological_variables(db, hapax_set)
     print('applying morphological variables to database')
-    new_data_by_prs = apply_morpho_vars_to_lex_db(segm_db, morpho_vars)
+    new_data_by_prs = apply_morpho_vars_to_lex_db(db, morpho_vars)
 
-    with open(DB_PATH) as database:
-        reader = csv.reader(database)
-        headers1 = next(reader)
-        headers2 = next(reader)
-        main_db = {x[1]:x for x in reader if len(x) > 1}
-        print(len(main_db))
-    print('merging new data with existing database')
-    merged_data = merge_new_data_with_database(new_data_by_prs, main_db)
+    # with open(DB_PATH) as database:
+    #     reader = csv.reader(database)
+    #     headers1 = next(reader)
+    #     headers2 = next(reader)
+    #     main_db = {x[1]:x for x in reader if len(x) > 1}
+    #     print(len(main_db))
+    # print('merging new data with existing database')
+    # merged_data = merge_new_data_with_database(new_data_by_prs, main_db)
 
     # create output directory if it doesn't exist already
     os.makedirs('output', exist_ok=True)   
-    for prs, data in merged_data.items():
+    for prs, data in new_data_by_prs.items():
         prs_str = re.sub(r'[,()]', '', str(prs))
         savepath = os.path.join(PROJECT_PATH, 'output/%s.csv' % prs_str)
         with open(savepath, 'w') as f:
